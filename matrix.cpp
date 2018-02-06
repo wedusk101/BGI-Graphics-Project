202226881532445979cf5e5f90d1af1e4c872b29
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-02-06 20:02:53 +0530

    Update

--- matrix.cpp
+++ matrix.cpp
@@ -3,13 +3,13 @@
 #include "primitives.h"
 #include "matrix.h"
 
-void rotate(Point & p, double theta)
+void rotate(Point & p, double theta) 
 {
 	p.x = (int) p.x * cos(theta) - p.y * sin(theta);
 	p.y = (int) p.x * sin(theta) + p.y * cos(theta);
 }
 
-void translate(Point & p, int tx, int ty)
+void translate(Point & p, int tx, int ty) 
 {
 	p.x = p.x + tx;
 	p.y = p.y + ty;
--- matrix.h
+++ matrix.h
@@ -5,7 +5,7 @@
 #include <cmath>
 #include "primitives.h"
 
-void rotate(Point & p, double theta);
-void translate(Point & p, int tx, int ty);
+void rotate(Point & p, double theta); // rotates a ray about the origin by an angle of theta radians
+void translate(Point & p, int tx, int ty); // translates a point P(x,y) by tx and ty 
 
 #endif
