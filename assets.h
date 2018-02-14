53d91f8f4d38bc9435e1efbcba800d2fc2f7823e
Arihant Kumar Seraogi <arihantseraogi@gmail.com>
2018-02-15 01:00:30 +0530

    Update assets.h

--- assets.h
+++ assets.h
@@ -15,8 +15,17 @@ namespace primitives
 		Rectangle rLeg;
 		Rectangle lLeg;
 	} Humanoid;
+	
+	typedef struct
+	{
+	    Point center;
+	    Line uString;
+	    Line lString;
+	} Bow;
 
 	Humanoid genHumanoid(Point p); //centre of the circle constituting the head is passed as argument
+	Bow genBow(Point P);           // the argument is the point at which the arc is centered at
+
 }
 
 #endif
