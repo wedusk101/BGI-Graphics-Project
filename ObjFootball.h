242700aaa336fc2ac0de7caf413f830cadd16f57
Arihant Kumar Seraogi <arihantseraogi@gmail.com>
2018-02-18 19:02:33 +0530

    Update ObjFootball.h

--- ObjFootball.h
+++ ObjFootball.h
@@ -5,6 +5,19 @@
 
 namespace primitives
 {
+    typedef struct
+	{
+		Circle head;
+		Rectangle neck;
+		Rectangle torso;
+		Rectangle rHand;
+	    Rectangle lHand;
+		Rectangle rLeg;
+		Rectangle lLeg;
+	} Humanoid;
+    
+    
+    Humanoid genHumanoid(const Point &); //centre of the circle constituting the head is passed as argument
     void football(const Circle &)
     void Rods(const int &,const int &);
 }
