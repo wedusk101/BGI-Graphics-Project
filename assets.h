0d0b2ff162946c2688efe1474162647626e2d50e
Arihant Kumar Seraogi <arihantseraogi@gmail.com>
2018-02-18 20:18:17 +0530

    Update assets.h

--- assets.h
+++ assets.h
@@ -14,8 +14,23 @@ namespace primitives
 		Line uString;
 		Line lString;
 	} Bow;
+	 typedef struct
+	{
+		Circle head;
+		Rectangle neck;
+		Rectangle torso;
+		Rectangle rHand;
+	        Rectangle lHand;
+		Rectangle rLeg;
+		Rectangle lLeg;
+	} Humanoid;
+    
+    
+        Humanoid genHumanoid(const Point &); //centre of the circle constituting the head is passed as argument
+        void football(const Circle &)
+        void Rods(const int &,const int &);
 
-	Humanoid genHumanoid(const Point &); //centre of the circle constituting the head is passed as argument
+	  
 	Bow genBow(const Point &);
 }
 
