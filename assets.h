d878336444d8603633565663be452803d00a2153
VishalAgarwal <vishalsunilagg@hotmail.com>
2018-02-21 00:05:11 +0530

    Target object for Archery

--- assets.h
+++ assets.h
@@ -26,15 +26,22 @@ namespace primitives
 		Rectangle lLeg;
 	} Humanoid;
     
+	typedef struct
+	{
+	    Line vert;
+	    Line horiz;
+	}Target;
     
-    Humanoid genHumanoid(const Point &); //center of the circle constituting the head is passed as argument
+    	Humanoid genHumanoid(const Point &); //center of the circle constituting the head is passed as argument
 	Bow genBow(const Point &);
 
 	Rectangle getRectangle(const int &, const int &, const int &, const int &);
 	
-    void genFootball(const Circle &);
-    void genRods(const int &, const int &);
-	void genGoalPost();	
+    	void genFootball(const Circle &);
+    	void genRods(const int &, const int &);
+	void genGoalPost();
+	Target genTarget(const Point &);    //Function to generate coordinates for the Target
+	void drawTarget(Target);            //Function to draw the Target
 }
 
 #endif
