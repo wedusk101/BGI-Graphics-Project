e3ac762133fc183bda1d80069748d60babab1c6e
VishalAgarwal <vishalsunilagg@hotmail.com>
2018-02-22 00:12:37 +0530

    Update assets.h

--- assets.h
+++ assets.h
@@ -10,38 +10,52 @@ namespace primitives
 {
 	typedef struct
 	{
-		Point center;
-		Line uString;
-		Line lString;
-	} Bow;
-	
-	typedef struct
-	{
 		Circle head;
 		Rectangle neck;
 		Rectangle torso;
 		Rectangle rHand;
-	    Rectangle lHand;
+	    	Rectangle lHand;
 		Rectangle rLeg;
 		Rectangle lLeg;
 	} Humanoid;
     
+	
+	
 	typedef struct
+	{
+		Point center;
+		Line uString;
+		Line lString;
+		int radius;
+		int midy;
+	}Bow;
+    
+    	typedef struct
+    	{
+        	Point p;
+        	int size;
+    	}Arrow;
+    
+    	typedef struct
 	{
 	    Line vert;
 	    Line horiz;
-	}Target;
+	}Target;		
     
-    	Humanoid genHumanoid(const Point &); //center of the circle constituting the head is passed as argument
-	Bow genBow(const Point &);
-
-	Rectangle getRectangle(const int &, const int &, const int &, const int &);
 	
+	
+    	Humanoid genHumanoid(const Point &); //center of the circle constituting the head is passed as argument
+	Rectangle getRectangle(const int &, const int &, const int &, const int &);
     	void genFootball(const Circle &);
     	void genRods(const int &, const int &);
 	void genGoalPost();
-	Target genTarget(const Point &);    //Function to generate coordinates for the Target
-	void drawTarget(Target);            //Function to draw the Target
+	
+	Bow genBow(const Point &);            //Function to generate coordinates for the Bow
+	void drawBow(Bow);                    //Function to draw the Bow
+	Arrow genArrow(const Point&,int &);   //Function to generate coordinates for the Arrow
+	void drawArrow(Arrow);                //Function to draw the Arrow
+	Target genTarget(const Point &);      //Function to generate coordinates for the Target
+	void drawTarget(Target);              //Function to draw the Target
 }
 
 #endif
