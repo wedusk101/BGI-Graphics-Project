984301505307a61c6c9dc085807d9c667af956a1
akash-ag <akash96.ag@gmail.com>
2018-03-12 14:46:30 +0530

    Update assets.cpp

--- assets.cpp
+++ assets.cpp
@@ -46,81 +46,83 @@ namespace primitives
 	}
 	
 	void genFootball(Point &center,int &radius)
-    	{
+    {
 
-    		Point point,poly1,poly2,poly3,poly4,poly5,poly6;
-        	setcolor(15); // Light Red
-		circle(ball.center.x, ball.center.y, ball.radius); //Circle drawn
+    Point poly1,poly2,poly3,poly4,poly5,poly6;
+        setcolor(15); // Light Red
+	circle(center.x, center.y,radius); //Circle drawn
 
-		poly1.x=ball.center.x-8;
-		poly1.y=ball.center.y-(ball.radius-1);
-		poly2.x=ball.center.x-4;
-		poly2.y=ball.center.y-(ball.radius-8);
-		poly3.x=ball.center.x+4;
-		poly3.y=ball.center.y-(ball.radius-8);
-		poly4.x=ball.center.x+8;
-		poly4.y=ball.center.y-(ball.radius-1);
-		int polygon2[8]={poly1.x,poly1.y,poly2.x,poly2.y,poly3.x,poly3.y,poly4.x,poly4.y};
-		drawpoly(4,polygon2);
+	poly1.x=center.x-8;
+	poly1.y=center.y-(radius-1);
+	poly2.x=center.x-4;
+	poly2.y=center.y-(radius-8);
+	poly3.x=center.x+4;
+	poly3.y=center.y-(radius-8);
+	poly4.x=center.x+8;
+	poly4.y=center.y-(radius-1);
+	int polygon2[8]={poly1.x,poly1.y,poly2.x,poly2.y,poly3.x,poly3.y,poly4.x,poly4.y};
+	drawpoly(4,polygon2);
 
-		poly1.x=ball.center.x+(ball.radius-1);
-		poly1.y=ball.center.y-8;
-		poly2.x=ball.center.x+(ball.radius-8);
-		poly2.y=ball.center.y-4;
-		poly3.x=ball.center.x+(ball.radius-8);
-		poly3.y=ball.center.y+4;
-		poly4.x=ball.center.x+(ball.radius-1);
-		poly4.y=ball.center.y+8;
+	poly1.x=center.x+(radius-1);
+	poly1.y=center.y-8;
+	poly2.x=center.x+(radius-8);
+	poly2.y=center.y-4;
+	poly3.x=center.x+(radius-8);
+	poly3.y=center.y+4;
+	poly4.x=center.x+(radius-1);
+	poly4.y=center.y+8;
 
-		int polygon3[8]={poly1.x,poly1.y,poly2.x,poly2.y,poly3.x,poly3.y,poly4.x,poly4.y};
-		drawpoly(4,polygon3);
+	int polygon3[8]={poly1.x,poly1.y,poly2.x,poly2.y,poly3.x,poly3.y,poly4.x,poly4.y};
+	drawpoly(4,polygon3);
 
 
-    		poly1.x=ball.center.x-(ball.radius-1);
-		poly1.y=ball.center.y-8;
-		poly2.x=ball.center.x-(ball.radius-8);
-		poly2.y=ball.center.y-4;
-		poly3.x=ball.center.x-(ball.radius-8);
-		poly3.y=ball.center.y+4;
-		poly4.x=ball.center.x-(ball.radius-1);
-		poly4.y=ball.center.y+8;
+    poly1.x=center.x-(radius-1);
+	poly1.y=center.y-8;
+	poly2.x=center.x-(radius-8);
+	poly2.y=center.y-4;
+	poly3.x=center.x-(radius-8);
+	poly3.y=center.y+4;
+	poly4.x=center.x-(radius-1);
+	poly4.y=center.y+8;
 
-		int polygon4[8]={poly1.x,poly1.y,poly2.x,poly2.y,poly3.x,poly3.y,poly4.x,poly4.y};
-		drawpoly(4,polygon4);
+	int polygon4[8]={poly1.x,poly1.y,poly2.x,poly2.y,poly3.x,poly3.y,poly4.x,poly4.y};
+	drawpoly(4,polygon4);
 
-    		poly1.x=ball.center.x-8;
-		poly1.y=ball.center.y+(ball.radius-1);
-		poly2.x=ball.center.x-4;
-		poly2.y=ball.center.y+(ball.radius-8);
-		poly3.x=ball.center.x+4;
-		poly3.y=ball.center.y+(ball.radius-8);
-		poly4.x=ball.center.x+8;
-		poly4.y=ball.center.y+(ball.radius-1);
-		int polygon5[8]={poly1.x,poly1.y,poly2.x,poly2.y,poly3.x,poly3.y,poly4.x,poly4.y};
-		drawpoly(4,polygon5);
+    poly1.x=center.x-8;
+	poly1.y=center.y+(radius-1);
+	poly2.x=center.x-4;
+	poly2.y=center.y+(radius-8);
+	poly3.x=center.x+4;
+	poly3.y=center.y+(radius-8);
+	poly4.x=center.x+8;
+	poly4.y=center.y+(radius-1);
+	int polygon5[8]={poly1.x,poly1.y,poly2.x,poly2.y,poly3.x,poly3.y,poly4.x,poly4.y};
+	drawpoly(4,polygon5);
 
-    		setfillstyle(SOLID_FILL,LIGHTGRAY);
-		floodfill(ball.center.x, ball.center.y,15);
+    setfillstyle(SOLID_FILL,LIGHTGRAY);
+	floodfill(center.x, center.y,15);
 
-    		poly1.x=ball.center.x-4;
-		poly1.y=ball.center.y-8;
-		poly2.x=ball.center.x+4;
-		poly2.y=ball.center.y-8;
-		poly3.x=ball.center.x+8;
-		poly3.y=ball.center.y;
-		poly4.x=ball.center.x+4;
-		poly4.y=ball.center.y+8;
-		poly5.x=ball.center.x-4;
-		poly5.y=ball.center.y+8;
-		poly6.x=ball.center.x-8;
-		poly6.y=ball.center.y;
-		int polygon[14]={poly1.x,poly1.y,poly2.x,poly2.y,poly3.x,poly3.y,poly4.x,poly4.y,poly5.x,poly5.y,poly6.x,poly6.y,poly1.x,poly1.y};
-		drawpoly(7,polygon);
+    poly1.x=center.x-4;
+	poly1.y=center.y-8;
+	poly2.x=center.x+4;
+	poly2.y=center.y-8;
+	poly3.x=center.x+8;
+	poly3.y=center.y;
+	poly4.x=center.x+4;
+	poly4.y=center.y+8;
+	poly5.x=center.x-4;
+	poly5.y=center.y+8;
+	poly6.x=center.x-8;
+	poly6.y=center.y;
+	int polygon[14]={poly1.x,poly1.y,poly2.x,poly2.y,poly3.x,poly3.y,poly4.x,poly4.y,poly5.x,poly5.y,poly6.x,poly6.y,poly1.x,poly1.y};
+	drawpoly(7,polygon);
 
-	 	setfillstyle(SOLID_FILL,RED);
-		floodfill(ball.center.x, ball.center.y,15);
+	 setfillstyle(SOLID_FILL,RED);
+	floodfill(center.x, center.y,15);
+	setlinestyle(0,0,1);
 
-    	}
+    }
+
 void drawRods(Rectangle upRod,Rectangle downRod)
 	{
         rectangle(upRod.tL.x,upRod.tL.y,upRod.bR.x,upRod.bR.y);
