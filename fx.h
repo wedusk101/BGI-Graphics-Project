074eafe1b42553115be0a3f2173724f514d671db
akash-ag <akash96.ag@gmail.com>
2018-03-13 13:26:05 +0530

    Update fx.h

--- fx.h
+++ fx.h
@@ -11,10 +11,11 @@ namespace primitives
 {
 	void shockWave(const Point &, const int &, const int &);
 	void showerConfetti(const int &, const int &, const double &, const double &, const int &, const int &, const int &); // WIP
+	void particleWave(const Point &, const int &, const int &, const int &, const int &);
 }
 
 // void boxBlur(const int &, const int &, const int &, const int &, const int &); // WIP
-//void ramp(starting point, ending point, type = linear/exponential, color); 
+//void ramp(starting point, ending point, type = linear/exponential, color);
 
 
 #endif
