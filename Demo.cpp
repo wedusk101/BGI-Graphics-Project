46200cfd073023c0e1b85d6bf8f0c34feda47be7
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-03-23 12:30:01 +0530

    Update

--- Demo.cpp
+++ Demo.cpp
@@ -14,7 +14,7 @@ int main()
 {
 	int score = 0;
 	std::string points; // for displaying the score
-	const double stepSize = 0.5; // arbitrary acceleration value - for g take 9.8
+	const double stepSize = 0.5; // tick rate for the physics simulations
 	double theta = 0.0, pointerTheta = -0.78539, pointerStep = 0.001, pointerLen = 0.0;
 	bool flag = true;
 		
